15b2b5894cde1cc2a537f98984d84fb2048853ac
Tim <0xtimc@gmail.com>
2022-04-20 12:52:53 +0100

    Initial Commit

---
+++ VaporDemo/VaporDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  VaporDemoApp.swift
+//  VaporDemo
+//
+//  Created by Tim Condon on 20/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct VaporDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
